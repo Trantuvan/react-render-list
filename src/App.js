92b2de9b931f97ae62d1d3eec7cccbbe7365eda6
trantuvan <trantuvan.kan@gmail.com>
2022-12-23 11:54:09 +0700

    change to class component & render a form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return <div className="App"></div>;
+export default class App extends Component {
+  state = {
+    tasks: [],
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form className="form">
+          <div className="form-group">
+            <label htmlFor="task" className="form-label">
+              Input Task
+            </label>
+            <input
+              id="task"
+              type="text"
+              placeholder="input here"
+              className="form-control"
+            />
+          </div>
+          <button type="submit" className="form-submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
-
-export default App;
