e82852a910b5c57ddcc94891e12d50d03fa36f04
trantuvan <trantuvan.kan@gmail.com>
2022-12-23 21:30:22 +0700

    edit field input

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default class App extends Component {
   state = {
     task: { id: uniqid(), text: "", order: 0 },
     tasks: [],
+    isEdit: false,
   };
 
   // submitHandler = (evt) => {
@@ -38,6 +39,23 @@ export default class App extends Component {
 
   onSubmitTask = (evt) => {
     evt.preventDefault();
+
+    if (this.state.isEdit === true) {
+      // const tasksWithOutEdited = this.state.tasks.filter(
+      //   (task) => task.id !== this.state.task.id
+      // );
+      // console.log(tasksWithOutEdited);
+      this.setState({
+        tasks: [
+          ...this.state.tasks,
+          // { ...this.state.task, id: uniqid(), text: this.state.task.text },
+          this.state.task,
+        ],
+        task: { id: uniqid(), text: "", order: this.state.task.order + 1 },
+        isEdit: false,
+      });
+    }
+
     this.setState({
       tasks: this.state.tasks.concat({
         ...this.state.task,
@@ -53,8 +71,18 @@ export default class App extends Component {
     });
   };
 
+  editTask = (taskId) => {
+    const input = document.querySelector("#task");
+    input.focus();
+    this.setState({
+      task: this.state.tasks.find((task) => task.id === taskId),
+      isEdit: true,
+      tasks: this.state.tasks.filter((task) => task.id !== taskId),
+    });
+  };
+
   render() {
-    const { tasks, task } = this.state;
+    const { tasks, task, isEdit } = this.state;
 
     return (
       <div className="App">
@@ -74,10 +102,14 @@ export default class App extends Component {
             />
           </div>
           <button type="submit" className="form-submit">
-            Submit
+            {isEdit ? "re-submit" : "submit"}
           </button>
         </form>
-        <Overview tasks={tasks} removeHandler={this.removeTask} />
+        <Overview
+          tasks={tasks}
+          removeHandler={this.removeTask}
+          editHandler={this.editTask}
+        />
       </div>
     );
   }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export default class Overview extends Component {
   render() {
-    const { tasks, removeHandler } = this.props;
+    const { tasks, removeHandler, editHandler } = this.props;
 
     return (
       <ul className="task-list">
@@ -12,6 +12,10 @@ export default class Overview extends Component {
             <i
               className="fa-solid fa-delete-left"
               onClick={() => removeHandler(task.id)}
+            ></i>
+            <i
+              className="fa-regular fa-pen-to-square"
+              onClick={() => editHandler(task.id)}
             ></i>
           </li>
         ))}
